ac0ae25da8dd4a02547eb1388d4ec371752b7182
RZFury <48477044+rzfury@users.noreply.github.com>
2022-07-28 15:10:35 +0700

    Initial Commit

---
+++ Game/Game.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace RZGame
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        public static Game Instance;
+
+        private GraphicsDeviceManager _gdm;
+        private SpriteBatch _sb;
+
+        public Game() : base()
+        {
+            _gdm = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = 1280,
+                PreferredBackBufferHeight = 720,
+                IsFullScreen = false
+            };
+            Instance = this;
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        protected override void OnExiting(object sender, EventArgs args)
+        {
+            base.OnExiting(sender, args);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+            _gdm.GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+    }
+}
---
+++ Game/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RZGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new())
+            {
+                game.Window.Title = "NEZ";
+                game.Run();
+            }
+        }
+    }
+}
